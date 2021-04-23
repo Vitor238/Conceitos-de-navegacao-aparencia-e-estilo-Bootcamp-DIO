4cd8a381dfd46edbdc69febf9cf9b77496bdd36a
Vitor Ferreira <vitorfer238@gmail.com>
2021-04-23 19:19:18 -0300

    recyclerview added

---
+++ app/src/main/java/com/vitor238/contatos/Contact.kt
@@ -0,0 +1,7 @@
+package com.vitor238.contatos
+
+data class Contact(
+    var name: String,
+    var phone: String,
+    var photograph: String
+)
--- app/src/main/java/com/vitor238/contatos/MainActivity.kt
+++ app/src/main/java/com/vitor238/contatos/MainActivity.kt
@@ -2,10 +2,43 @@ package com.vitor238.contatos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindView()
+        updateList()
+    }
+
+    private fun bindView() {
+        rvList.adapter = adapter
+        rvList.layoutManager = LinearLayoutManager(this)
+    }
+
+    private fun updateList() {
+        adapter.updateList(
+            arrayListOf(
+                Contact(
+                    "Igor Ferrani",
+                    "(00) 00000-0000",
+                    "img.png"
+                ),
+                Contact(
+                    "Igor Ferrani",
+                    "(00) 00000-0000",
+                    "img.png"
+                )
+            )
+        )
     }
 }
